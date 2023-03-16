666f56ca489383ebd667f31c3691ef4a265a33d8
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2023-03-16 14:34:33 +0100

    Initial commit

---
+++ news.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="green">News</h1>
+    <p>This is the news page</p>
+  </div>
+</template>
---
+++ users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="green">Users</h1>
+    <p>This is the users page</p>
+    <span v-for="user in users" v-bind:key="user.id"> {{ user.name }} </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const users = ref([
+  {
+    id: 1,
+    name: "Ole",
+  },
+  {
+    id: 1,
+    name: "Adam",
+  },
+  {
+    id: 1,
+    name: "Eva",
+  },
+  {
+    id: 1,
+    name: "Test",
+  },
+]);
+</script>
