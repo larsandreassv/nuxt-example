2c2c9b99b4c03bcbff867365b32775a160c7b40a
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2023-03-16 16:23:52 +0100

    Changed color of products header

--- frontend/pages/products.vue
+++ frontend/pages/products.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-  <h1 class="green">Products</h1>
-  <p>This is the products page</p>
-</div>
+  <div>
+    <h1 class="text-red">Products</h1>
+    <p>This is the products page</p>
+  </div>
 </template>
