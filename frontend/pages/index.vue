b30cad9e479222738adfc4820af814c5c09c1d6e
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2023-03-17 11:30:11 +0100

    Update

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col">
     <NuxtLink to="/news">News</NuxtLink>
-    <NuxtLink to="/products">Products</NuxtLink>
+    <NuxtLink to="/books">Books</NuxtLink>
     <NuxtLink to="/users">Users</NuxtLink>
   </div>
 </template>
