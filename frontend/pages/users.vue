64ee56f886ff52f4946f3d1b22056096a70ee32f
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2023-03-17 13:06:40 +0100

    Update

--- frontend/pages/users.vue
+++ frontend/pages/users.vue
@@ -3,7 +3,7 @@
     <h1 class="text-xl text-green-900">Users</h1>
     <p>This is the users page</p>
     <h2 class="text-blue-900">List of users:</h2>
-    <span class="flex flex-col text-red-700" v-for="user in users" v-bind:key="user.id"> {{ user.name }} </span>
+    <span class="flex flex-col" v-for="user in users" v-bind:key="user.id"> {{ user.name }} </span>
   </div>
 </template>
 
