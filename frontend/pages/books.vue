bc89d9c383494f6683c3c86db72a9ef079570bd9
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2023-03-17 11:06:23 +0100

    Added books page

---
+++ frontend/pages/books.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <span v-for="book in books" v-bind:key="book.id"> {{ book }}</span>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from "vue";
+  
+  const books = ref([
+    {
+      id: 1,
+      value: "Harry potter and the goblet of fire",
+    },
+    {
+      id: 2,
+      value: "Dune",
+    },
+    {
+      id: 3,
+      value: "The Very Hungry Caterpillar",
+    },
+    {
+      id: 4,
+      value: "Lord of the rings",
+    },
+  ]);
+  </script>
+  
